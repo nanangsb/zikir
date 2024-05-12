f733fcaafaf7801ee212e6e9efb96b1cab625bb2
nanangsb <sefida.nanang@gmail.com>
2024-05-12 12:31:12 +0700

    Update icons.ts

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -4,6 +4,10 @@ import selectedDua from '$assets/selected-dua.png'
 import morningDhikr from '$assets/morning-dhikr.png'
 import eveningDhikr from '$assets/evening-dhikr.png'
 import dhikrAfterSalah from '$assets/dhikr-after-salah.png'
+import hajiUmroh from '$assets/hajj.png'
+import wudhu from '$assets/wudhu.png'
+import shalat from '$assets/shalat.png'
+
 
 export const icons = (slug: string) => {
   const icons = {
@@ -12,7 +16,9 @@ export const icons = (slug: string) => {
     'morning-dhikr': morningDhikr,
     'evening-dhikr': eveningDhikr,
     'dhikr-after-salah': dhikrAfterSalah,
-    'haji-umroh': selectedDua,
+    'haji-umroh': hajiUmroh,
+    'wudhu': wudhu,
+    'sholat': shalat,
   }
 
   return icons[slug] || noIcon
